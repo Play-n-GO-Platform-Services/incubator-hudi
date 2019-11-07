e1ff2814ba3eaa5566423f8618b726dbf74a3007
hariprasad.allaka@playngo.com <hariprasad.allaka@playngo.com>
2019-11-07 16:40:58 +0100

    Writing custom partitioner for playngo

--- hudi-hive/src/main/java/org/apache/hudi/hive/SlashEncodedDayPartitionValueExtractor.java
+++ hudi-hive/src/main/java/org/apache/hudi/hive/SlashEncodedDayPartitionValueExtractor.java
@@ -19,10 +19,13 @@
 package org.apache.hudi.hive;
 
 import com.beust.jcommander.internal.Lists;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+
 
 /**
  * HDFS Path contain hive partition values for the keys it is partitioned on. This mapping is not
@@ -48,28 +51,12 @@ public class SlashEncodedDayPartitionValueExtractor implements PartitionValueExt
 
   @Override
   public List<String> extractPartitionValuesInPath(String partitionPath) {
-    // partition path is expected to be in this format yyyy/mm/dd
     String[] splits = partitionPath.split("/");
-    if (splits.length > 3) {
-      String opName = splits[0];
-      int year = Integer.parseInt(splits[1]);
-      int mm = Integer.parseInt(splits[2]);
-      int dd = Integer.parseInt(splits[3]);
-      DateTime dateTime = new DateTime(year, mm, dd, 0, 0);
-      return Lists.newArrayList(opName, getDtfOut().print(dateTime));
-    } else if (splits.length == 3) {
-      // Get the partition part and remove the / as well at the end
-      int year = Integer.parseInt(splits[0]);
-      int mm = Integer.parseInt(splits[1]);
-      int dd = Integer.parseInt(splits[2]);
-      DateTime dateTime = new DateTime(year, mm, dd, 0, 0);
-      return Lists.newArrayList(getDtfOut().print(dateTime));
+    ArrayList<String> al = new ArrayList<String>();
+    for(int i = 0; i <= splits.length -1 ; i++ ){
+        al.add(splits[i]);
     }
-    else{
-      throw new IllegalArgumentException(
-              "Partition path " + partitionPath + " is not in the form yyyy/mm/dd or opname/yyyy/mm/dd");
-    }
-
+    return Lists.newArrayList(al);
   }
 
 }
